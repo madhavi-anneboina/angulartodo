fa0b7731c9a8537fb52fe685c2a8f42cd8abb6e3
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-05-08 12:53:15 -0400

    pipes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,43 +14,7 @@ export class AppComponent {
   constructor(private fb:FormBuilder){
    
   }
-  regForm = new FormGroup({
-    username: new FormControl(null, [Validators.required], [this.usernameCheck.bind(this)])
-  });
 
-
- usernameCheck =(control:any):Promise<any> =>{
-   return new Promise ((resolve,reject)=>{
-   let users = [
-      "user1","user2","user3"
-     ]
-    let name = control.value; 
-    setTimeout(()=>{
-      if(users.indexOf(name)>=0){
-        resolve({"duplicateuser":true})
-      } else{
-        resolve (null)
-      }
-    },2000)
-
-    }
-  )
- }
-
- uppercaseTest(c:any){
-  let pattern = /[A-Z]/
-  if(pattern.test(c.value)){
-    return null
-  
-  }
-  return {'noupper':true}
- }
-
- show(){
-  console.log(this.regForm)
- }
-
-  
 
   }
 
