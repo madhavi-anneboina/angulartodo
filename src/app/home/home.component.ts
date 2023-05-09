65a5ce1b3c462054e23ef561b866be01b7ae3170
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-05-09 15:47:23 -0400

    ng onchange life cycle method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnChanges{
+  constructor(){}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ng on changes implemented ")
+  }
 
 }
