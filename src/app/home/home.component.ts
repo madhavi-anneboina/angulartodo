a7060fd5c58f8f4b64528e4ee46ec651af7c050a
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-05-10 16:02:22 -0400

    ngOninit life cycle method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,20 @@
-import { Component,OnChanges, SimpleChanges,Input } from '@angular/core';
+import { Component,OnChanges, SimpleChanges,Input,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnChanges{
+export class HomeComponent implements OnInit {
   @Input() cdata:any
-  constructor(){}
 
+ngOnInit(): void {
+  console.log("onInit called ")
+}
+constructor(){
+  console.log("constructor callde")
+
+}
   ngOnChanges(changes: SimpleChanges): void {
     console.log("ng on changes implemented ")
     console.log(changes)
