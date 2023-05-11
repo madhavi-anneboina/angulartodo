158976e27eb0ce08114fac230963233ca270495d
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-05-11 14:41:56 -0400

    ngcontentchecked life cycle method added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
-import { Component,OnChanges, SimpleChanges,Input,OnInit,DoCheck } from '@angular/core';
+import { Component,OnChanges, SimpleChanges,Input,OnInit,DoCheck,AfterContentChecked,AfterContentInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit,DoCheck,OnChanges {
+export class HomeComponent implements OnInit,DoCheck,OnChanges,AfterContentChecked,AfterContentInit {
   @Input() cdata:any
   @Input() parentdata="child wish "
   @Input() mobiles:any
@@ -22,9 +22,16 @@ ngDoCheck(): void {
   
   
 }
+ngAfterContentChecked(): void {
+  console.log("content checked is called ")
+}
+ngAfterContentInit(): void {
+  console.log("After content init called")
+}
 ngOnInit(): void {
  
 }
+
 constructor(){
   
 
