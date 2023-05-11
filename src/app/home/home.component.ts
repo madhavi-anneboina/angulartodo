402174a7a81808a52af096bb5a966d4e3d53ca46
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-05-11 15:17:12 -0400

    ng on destroy life cycle method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
-import { Component,OnChanges, SimpleChanges,Input,OnInit,DoCheck,AfterContentChecked,AfterContentInit} from '@angular/core';
+import { Component,OnChanges, SimpleChanges,Input,OnInit,DoCheck,AfterContentChecked,AfterContentInit,OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit,DoCheck,OnChanges,AfterContentChecked,AfterContentInit {
+export class HomeComponent implements OnInit,DoCheck,OnChanges,AfterContentChecked,AfterContentInit,OnDestroy {
   @Input() cdata:any
   @Input() parentdata="child wish "
   @Input() mobiles:any
@@ -22,11 +22,14 @@ ngDoCheck(): void {
   
   
 }
+ngOnDestroy(): void {
+  console.log("Home componet destored")
+}
 ngAfterContentChecked(): void {
-  console.log("content checked is called ")
+  // console.log("content checked is called ")
 }
 ngAfterContentInit(): void {
-  console.log("After content init called")
+  // console.log("After content init called")
 }
 ngOnInit(): void {
  
