16029a9d3c911faa743694cfce02d8bb2fef961c
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-05-18 15:36:02 -0400

    app componemt changes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,16 +21,7 @@ export class AppComponent  {
     "name": "Clementine Bauch",
     "username": "Samantha",
     "email": "Nathan@yesenia.net",
-    "address": {
-      "street": "Douglas Extension",
-      "suite": "Suite 847",
-      "city": "McKenziehaven",
-      "zipcode": "59590-4157",
-      "geo": {
-        "lat": "-68.6102",
-        "lng": "-47.0653"
-      }
-    },
+  
     "phone": "1-463-123-4447",
     "website": "ramiro.info",
     "company": {
