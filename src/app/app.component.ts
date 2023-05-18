8a4dd58615f8560975216f58f17ed1e84cb29fb1
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-05-18 15:37:44 -0400

    app.json()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,23 +21,10 @@ export class AppComponent  {
     "name": "Patricia Lebsack",
     "username": "Karianne",
     "email": "Julianne.OConner@kory.org",
-    "address": {
-      "street": "Hoeger Mall",
-      "suite": "Apt. 692",
-      "city": "South Elvis",
-      "zipcode": "53919-4257",
-      "geo": {
-        "lat": "29.4572",
-        "lng": "-164.2990"
-      }
-    },
+   
     "phone": "493-170-9623 x156",
     "website": "kale.biz",
-    "company": {
-      "name": "Robel-Corkery",
-      "catchPhrase": "Multi-tiered zero tolerance productivity",
-      "bs": "transition cutting-edge web services"
-    }
+   
   },
   {
     "id": 5,
