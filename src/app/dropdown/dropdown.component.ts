46ce8615519e6f80fbfc0519ad8ccdfaa49d0186
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-05-26 14:14:35 -0400

    array of states with ids added

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -27,6 +27,109 @@ export class DropdownComponent {
         name : "Karnataka",
         code : "KS"
       },
+]
+districts = [
+  {
+    id : 1,
+    name: "Krishna",
+    s_id : 1
+  },
+  {
+    id : 2,
+    name: "East Godavari",
+    s_id : 1
+  },
+  {
+    id : 3,
+    name: "WestbGodavari",
+    s_id : 1
+  },
+  {
+    id : 4,
+    name: "Guntur",
+    s_id : 1
+  },
+  {
+    id : 5,
+    name: "Vishakaptanam",
+    s_id : 1
+  },
+  {
+    id : 6,
+    name: "warangal",
+    s_id : 2
+  },
+  {
+    id : 7,
+    name: "Hyderabad",
+    s_id : 2
+  },
+  {
+    id : 8,
+    name: "Karimnagar",
+    s_id : 2
+  },
+  {
+    id : 9,
+    name: "Medak",
+    s_id : 2
+  },
+  {
+    id : 10,
+    name: "Suryapet",
+    s_id : 2
+  },
+  {
+    id : 11,
+    name: "Chennai",
+    s_id : 3
+  },
+  {
+    id : 12,
+    name: "Maduri",
+    s_id : 3
+  },
+  {
+    id : 13,
+    name: "coimbature",
+    s_id : 3
+  },
+  {
+    id : 14,
+    name: "Vellore",
+    s_id : 3
+  },
+  {
+    id : 15,
+    name: "Tanjavur",
+    s_id : 3
+  },
+  {
+    id : 16,
+    name: "Maisure",
+    s_id : 4
+  },
+  {
+    id : 17,
+    name: "Raichur",
+    s_id : 4
+  },
+  {
+    id : 18,
+    name: "Bengulur",
+    s_id : 4
+  },
+  {
+    id : 19,
+    name: "Ballari",
+    s_id : 4
+  },
+  {
+    id : 20,
+    name: "nachalakonda",
+    s_id : 4
+  },
 
-     ]
+
+]
 }
