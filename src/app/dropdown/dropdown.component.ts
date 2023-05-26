25c63881b4a04edb9c1323b6c01ebca8e3f2ddc8
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-05-26 13:59:24 -0400

    array of states added

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./dropdown.component.css']
 })
 export class DropdownComponent {
+     states = [
+      {
+        id : 1,
+        name : "Andhra Pradesh",
+        code : "AP"
+      },
+      {
+        id : 2,
+        name : "Telangana",
+        code : "TS"
+      },
+      {
+        id : 3,
+        name : "Tamil Nadu",
+        code : "TN"
+      },
+      {
+        id : 4,
+        name : "Karnataka",
+        code : "KS"
+      },
 
+     ]
 }
