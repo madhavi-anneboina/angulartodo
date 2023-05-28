df51d64912f8d70598419c9b4f4409d3762ea9b1
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-05-28 19:49:46 -0400

    added checkbox data

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { FormControl,FormGroup,FormBuilder,FormArray } from '@angular/forms';
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.css']
 })
 export class CheckboxComponent {
+  testForm:any;
+  langs = ["c","java","python","javascript","typescript","Angular","React"]
+  constructor(){
+
+  }
 
 }
