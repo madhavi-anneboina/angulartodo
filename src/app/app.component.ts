f9b3a39a81b19730e302732b0cab126431f6b504
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-05-30 22:38:01 -0400

    show and hide header added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http'
 import { FormControl,FormGroup, Validators,FormArray,FormBuilder} from '@angular/forms';
 import { Observable, of,from,range,interval,map,filter,fromEvent,debounceTime,merge,take,pluck,skip} from 'rxjs';
 import { TodoService } from './todo.service';
+import { NavigationEnd, Router } from '@angular/router';
 
 
 @Component({
@@ -13,7 +14,22 @@ import { TodoService } from './todo.service';
 export class AppComponent  {
   @ViewChild('mobile')m:any
   @ViewChild('text')ip:any
-  
+  showHeader:boolean = true
+   constructor(private router:Router){
+    router.events.subscribe(
+      (val) => {
+        if(val instanceof NavigationEnd){
+          if(val.url == '/login'){
+            this.showHeader = false 
+          } else{
+            this.showHeader = true 
+          }
+        }
+      }
+
+    )
+
+   }
  
  ngOnInit(): void {
 
