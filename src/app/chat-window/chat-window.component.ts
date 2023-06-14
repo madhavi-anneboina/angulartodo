27854fc23b08d90df00aa3cf82af4dd0b32b6602
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-06-13 21:21:21 -0400

    caht window component data added

--- src/app/chat-window/chat-window.component.ts
+++ src/app/chat-window/chat-window.component.ts
@@ -1,10 +1,14 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat-window',
   templateUrl: './chat-window.component.html',
-  styleUrls: ['./chat-window.component.css']
 })
 export class ChatWindowComponent {
+  messages: string[] = [];
 
+  receiveMessage(message: string) {
+    this.messages.push(message);
+  }
 }
