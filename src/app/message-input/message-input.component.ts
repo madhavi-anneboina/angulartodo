4e7ee9c1505e82b2c47b17fc5a86238a801c9db5
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-06-13 21:22:41 -0400

    message input component data added

--- src/app/message-input/message-input.component.ts
+++ src/app/message-input/message-input.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ChatWindowComponent } from '../chat-window/chat-window.component';
 
 @Component({
   selector: 'app-message-input',
   templateUrl: './message-input.component.html',
-  styleUrls: ['./message-input.component.css']
 })
 export class MessageInputComponent {
+  messageText: string = '';
 
+  constructor(private chatWindow: ChatWindowComponent) {}
+
+  sendMessage() {
+    this.chatWindow.receiveMessage(this.messageText);
+    this.messageText = '';
+  }
 }
