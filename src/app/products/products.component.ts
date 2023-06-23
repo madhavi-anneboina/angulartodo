48971f5cc57aece93784d3e1bf855f21e37b533b
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-06-23 14:26:39 -0400

    added usersdata

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,4 +7,70 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
+  users = [
+    {
+      "Id" : 1,
+      "Name": "Madhavi Anneboina",
+      "Email":"madhavi.anneboina@gmail.com",
+      "Mobile":"945734124721",
+      "State":"Andhrapradesh"
+    },
+    {
+      "Id" : 2,
+      "Name": "Mad Anneboina",
+      "Email":"madh.anneboina@gmail.com",
+      "Mobile":"578393021392",
+      "State":"Andhrapradesh"
+    },
+    {
+      "Id" : 3,
+      "Name": "Vani Anneboina",
+      "Email":"vani.anneboina@gmail.com",
+      "Mobile":"2448414344",
+      "State":"Andhrapradesh"
+    },
+    {
+      "Id" : 4,
+      "Name": "Rani Anneboina",
+      "Email":"rani.anneboina@gmail.com",
+      "Mobile":"242144",
+      "State":"Telangana"
+    },
+    {
+      "Id" : 5,
+      "Name": "kini Anneboina",
+      "Email":"kini.anneboina@gmail.com",
+      "Mobile":"442352354",
+      "State":"Karnataka"
+    },
+    {
+      "Id" : 6,
+      "Name": "jani Anneboina",
+      "Email":"jani.anneboina@gmail.com",
+      "Mobile":"7387329932",
+      "State":"Tamilanadu"
+    },
+    {
+      "Id" : 7,
+      "Name": "jos Anneboina",
+      "Email":"jos.anneboina@gmail.com",
+      "Mobile":"43432325532532",
+      "State":"antapur"
+    },
+    {
+      "Id" : 8,
+      "Name": "loni Anneboina",
+      "Email":"loni.anneboina@gmail.com",
+      "Mobile":"3325544646",
+      "State":"Uttaranchal"
+    },
+    {
+      "Id" : 9,
+      "Name": "koniru Anneboina",
+      "Email":"koniru.anneboina@gmail.com",
+      "Mobile":"3434872313",
+      "State":"Tamilanadu"
+    },
+  ]
+
 }
