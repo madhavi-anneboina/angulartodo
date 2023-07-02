358c39b9071b94efa4da6c2a160d4528f3e49c9e
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-07-02 17:30:15 -0400

    added label validation

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,30 +1,28 @@
-import { Component } from '@angular/core';
-import { FormBuilder,FormGroup,FormArray,Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.css']
 })
-export class RegistrationFormComponent {
-  regForm:FormGroup
- constructor(private fb:FormBuilder){
-  this.regForm = this.fb.group({
-    firstName:['',[Validators.required]],
-    lastName:['',[Validators.required]],
-    email:['',[Validators.required,Validators.email]],
-    password:['',[Validators.required,Validators.minLength(8)]],
-    confirmPassword:['',[Validators.required]],
+export class RegistrationFormComponent implements OnInit {
+  regForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.regForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]],
+    });
   }
 
-  )
+  ngOnInit(): void {
+  }
 
- }
- ngOninit(): void{
-
- }
- submit(){
-  console.log(this.regForm.value)
- }
+  submit() {
+    console.log(this.regForm.value);
+  }
 }
