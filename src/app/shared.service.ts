9e8df93e982126d9b01745b477a304c3f71abeb7
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-07-05 14:48:29 -0400

    added service

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -6,4 +6,11 @@ import { Injectable } from '@angular/core';
 export class SharedService {
 
   constructor() { }
+  todos = ['todo1','todo2']
+  getTodos(){
+    return this.todos
+  }
+  addTodo(task:any){
+    this.todos.push(task)
+  }
 }
