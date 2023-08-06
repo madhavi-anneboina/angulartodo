ceebd9344c05235691b85e53c6328e35618cc5a9
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-06 18:10:47 -0400

    added script file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,60 +11,18 @@ import { NavigationEnd, Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  @ViewChild('mobile')m:any
-  @ViewChild('text')ip:any
-  imageUrl: string = 'assets/image.jpeg';
- 
-  showHeader:boolean = true
-   constructor(private router:Router){
-    router.events.subscribe(
-      (val) => {
-        if(val instanceof NavigationEnd){
-          if(val.url == '/login'){
-            this.showHeader = false 
-          } else{
-            this.showHeader = true 
-          }
-        }
-      }
 
-    )
+export class AppComponent  {
+
+
+
     
    }
-   uploads: string[] = [];
-   saveImages(e:any){
-    console.log(e)
-    let totalFiles = e.target.files
-    if(totalFiles.length>0){
-      for(let i=0; i<totalFiles.length;i++){
-        let file = totalFiles[i]
-        const reader = new FileReader();
-        reader.readAsDataURL(file)
-        reader.onload = (e:any)=>{
-         let url:any = e.target.result;
-         this.uploads.push(url)
-        }
-      }
-      e.target.value=""
-    }
-   }
-   deleteImage(i:any){
-    this.uploads.splice(i,1)
-   }
- 
- ngOnInit(): void {
-
-
- }
-
-
-
-
-
+   
+     
+  
  
   
-  }
 
 
 
---
+++ src/app/script.js
@@ -0,0 +1,3 @@
+let millisecons = 0, seconds = 0, mintus = 0, hours = 0
+let timeshow = document.getElementById('timeshow')
+let timeref; 
