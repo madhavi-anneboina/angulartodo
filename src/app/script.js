adbedc30b709a9ff73040c30970bcc77ee8f620b
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-06 18:47:53 -0400

    added upadte timer logic

--- src/app/script.js
+++ src/app/script.js
@@ -1,4 +1,4 @@
-let millisecons = 0, seconds = 0, mintus = 0, hours = 0
+let milliseconds = 0, seconds = 0, mintues = 0, hours = 0
 let timeshow = document.getElementById('timeshow')
 let timeref; 
 
@@ -9,6 +9,20 @@ document.getElementById('start').onclick = function(){
 }
 
 function updateTime(){
-    
+    milliseconds = milliseconds + 10 ;
+    if(milliseconds == 1000){
+        seconds = seconds + 1
+        milliseconds = 0
+        if(seconds == 60){
+            mintues = mintues + 1
+            seconds = 0
+            if(mintues == 60){
+                hours = hours + 1 
+                mintues = 0
+            }
+
+        }
+
+    }
 }
 
