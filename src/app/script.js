51f07ba4233562c6a9133d919bdd54a3f77e337a
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-06 19:32:14 -0400

    added all functionalities

--- src/app/script.js
+++ src/app/script.js
@@ -24,5 +24,44 @@ function updateTime(){
         }
 
     }
+
+
+let h,m,s,ms;
+if(hours<10){
+    h = '0' + hours
+} else {
+    h = hours
+} 
+if(mintues<10){
+    m = "0" + mintues
+}else{
+    m = mintues
+} 
+if(seconds<10){
+    s = "0" + seconds
+} else {
+    s = seconds
+}
+if(milliseconds<10){
+    ms = '0' + milliseconds
+} else if(milliseconds<100){
+    ms = '00' + milliseconds
+}
+else {
+    ms = milliseconds
+}
+timeshow.innerHTML = `${h} : ${m} : ${s} :${ms}`
 }
 
+document.getElementById("pause").onclick = function(){
+    clearInterval(timeref)
+}
+
+document.getElementById("reset").onclick = function(){
+    clearInterval(timeref)
+    milliseconds = 0
+    seconds = 0
+    mintues = 0
+    hours = 0
+    timeshow.innerHTML = "00 :00: 00: 00"
+}
