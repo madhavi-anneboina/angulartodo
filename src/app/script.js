45f396679760756e4cab8dd598704bd3608481db
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-06 18:13:21 -0400

    added data

--- src/app/script.js
+++ src/app/script.js
@@ -1,3 +1,14 @@
 let millisecons = 0, seconds = 0, mintus = 0, hours = 0
 let timeshow = document.getElementById('timeshow')
 let timeref; 
+
+
+document.getElementById('start').onclick = function(){
+    timeref = setInterval(updateTime,10)
+
+}
+
+function updateTime(){
+    
+}
+
