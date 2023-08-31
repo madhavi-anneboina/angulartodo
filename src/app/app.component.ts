f5d7aba349f6193c23820087ed67a04ec70f330c
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-31 17:20:44 -0400

    template reference variable data added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,11 @@ import { NavigationEnd, Router } from '@angular/router';
 export class AppComponent {
 
 wish:any = "Hello good moring how are you doing today"
+email=""
+updateEmail(data:any){
+  this.email = data.value
 
+}
 
 }
 
