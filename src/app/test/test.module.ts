0886ee6ac2a4245c1f22c1fa9ac1b39a541fe5fd
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-08-31 15:53:32 -0400

    test pipes added

--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Test1Component } from './test1/test1.component';
+import { TestPipe } from '../test.pipe';
 
 
 
 @NgModule({
   declarations: [
-    Test1Component
+    Test1Component,
+    TestPipe
   ],
   imports: [
     CommonModule
