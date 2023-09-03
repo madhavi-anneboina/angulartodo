f9a0dc1ac69f5ec1214efa44815196c3772c09bf
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-09-03 13:14:12 -0400

    user service added

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -6,4 +6,45 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   constructor() { }
+  users = [
+    {
+       "id" : 1,
+      "firstName": "John",
+      "lastName": "Smith",
+      "isAlive": true,
+      "age": 27,
+      "address": {
+        "streetAddress": "21 2nd Street",
+        "city": "New York",
+        "state": "NY",
+        "postalCode": "10021-3100"
+      }
+    },
+    {
+      "id" : 2,
+     "firstName": "kohn",
+     "lastName": "kmith",
+     "isAlive": true,
+     "age": 27,
+     "address": {
+       "streetAddress": "21 2nd Street",
+       "city": "white York",
+       "state": "GP",
+       "postalCode": "10021-3100"
+     },
+   },
+   {
+    "id" : 3,
+   "firstName": "Mohn",
+   "lastName": "mmith",
+   "isAlive": true,
+   "age": 37,
+   "address": {
+     "streetAddress": "21 2nd Street",
+     "city": "white York",
+     "state": "GP",
+     "postalCode": "3000021-3100"
+   },
+  }
+  ]
 }
