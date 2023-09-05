dc6d794d7cc907601220295a2b6b26a5ebe088a7
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2023-09-05 13:27:31 -0400

    added html data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,62 +14,17 @@ import { NavigationEnd, Router } from '@angular/router';
 })
 
 export class AppComponent {
-  // @ViewChild("para") p:any
-
-  // @ViewChildren("heading") h:any
+  mobiles:any=[]
   constructor(private http: HttpClient) {
   }
-  // data:any = null
-  // getData(){
-  //   this.http.get('https://fakestoreapi.com/products?limit=5')
-  //   .subscribe((data)=>{
-  //     this.data = data
-  //   })
-  // }
 
-  // test(){
-  //   console.log(this.p)
-  //   for(let ele of this.h){
-  //     console.log(ele.nativeElement.innerText)
-  //   }
-  // }
-
-  myobs = new Observable(
-    (listner) => {
-      listner.next(1);
-      listner.next(2);
-      setTimeout(() => listner.next(3), 1000);
-      setTimeout(() => listner.next(4), 1000);
-      setTimeout(() => listner.next(6), 1000);
-      setTimeout(() => listner.complete(), 1000);
+ 
 
 
-    }
-  )
-
-  test(){
-    this.myobs.subscribe(
-      data => {console.log(data)},
-      err => {console.log(err)},
-      () => {console.log('completed')}
-        )
-  }
 
 
-oneobs = new Observable((hello)=>{
-   hello.next("How are you doing")
-   setTimeout(()=> hello.next("im doing good"),1000)
-   hello.next("what are you doing for living")
-   hello.error("Error is coming")
-   hello.complete()
-})
- secondtest(){
-   this.oneobs.subscribe(
-    data=>{console.log(data)},
-    err => {console.log(err)},
-    ()=> {console.log("completed")}
-   )
- }
+
+
 
 
 }
